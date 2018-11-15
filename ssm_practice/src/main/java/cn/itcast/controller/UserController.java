67cf5534e345410e6e6f61130876bb0bd6946d1b
crowndint <crowndint@163.com>
2018-11-15 15:05:00 +0800

    add,update

--- ssm_practice/src/main/java/cn/itcast/controller/UserController.java
+++ ssm_practice/src/main/java/cn/itcast/controller/UserController.java
@@ -1,5 +1,18 @@
+package cn.itcast.controller;
 public class UserController {
 
+
+    public void add() {
+
+
+
+        System.out.println("add ");
+    }
+    public void update() {
+
+
+        System.out.println("update");
+    }
 
 
 }
